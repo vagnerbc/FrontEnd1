52670dbd42f6ac575e274ee7a0bd4b8391ea91db
Your Name <you@example.com>
2016-07-25 22:49:33 -0300

    Criação do projeto no WebStorm

---
+++ ProjHtml/Scripts/script1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Vagner BC on 21/07/16.
+ */
+function ativar(){
+    document.getElementById("home").style.backgroundColor = "#4CAF50";
+}
---
+++ ProjHtml/Scripts/script2.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Vagner BC on 25/07/16.
+ */
+$(document).ready(function(){
+    $(".icon-bar a").click(function(){
+       $(this).toggleClass("active");
+    });
+});
